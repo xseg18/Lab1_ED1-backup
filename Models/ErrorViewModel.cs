9cd3dd889cea2a005f6bafc9a4ffcd641a353969
Ximena Elizardi <ximenagobern@gmail.com>
2021-02-12 08:56:10 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab1_ED1__backup_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
